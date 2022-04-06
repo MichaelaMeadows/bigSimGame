eb72088a81c3a8b15a29d3cea6d6465255b16f6f
MichaelaMeadows <michael.meadows09@gmail.com>
2022-04-06 12:03:35 -0400

    Adding main game files

---
+++ LearningSimGame/WorldState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningSimGame
+{
+    public class WorldState
+    {
+        static int[,] worldMap = new int[100,100];
+
+
+        public int[,] getMap()
+        {
+            return worldMap;
+        }
+
+    }
+}
